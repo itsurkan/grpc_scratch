c66d8658dc0261b53f857007f2804366ea2e1612
itsurkan <itsurkan.1@gmail.com>
2019-11-04 18:00:38 +0200

    Move grpc server, add client

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Net.Client;
+using grpc_scratch.Proto;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace gRPC_Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient(
+               new HttpClientHandler()
+               {
+                   ServerCertificateCustomValidationCallback =
+                    HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+               });
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001",
+                new GrpcChannelOptions { HttpClient = httpClient });
+
+            var client = new CustomerService.CustomerServiceClient(channel);
+
+            var request = new CustomerIdRequest() { CustId = 1 };
+            var reply = await client.GetCustomerByIdAsync(request);
+            Console.WriteLine(reply.FullName);
+
+            Console.ReadLine();
+        }
+    }
+}
