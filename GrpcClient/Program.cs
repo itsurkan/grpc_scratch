dcee612654f8c34a54a5c1ebb703a11fb990f92a
ivan_tsurkan1 <itsurkan.1@gmail.com>
2020-03-12 15:00:41 +0200

    Add logging

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -3,22 +3,31 @@ using grpc_scratch.Proto;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace gRPC_Client
 {
     class Program
     {
+        private ILoggerFactory _loggerFactory;
         static async Task Main(string[] args)
         {
-            var httpClient = new HttpClient(
-               new HttpClientHandler()
-               {
-                   ServerCertificateCustomValidationCallback =
-                    HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
-               });
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddConsole();
 
+                builder.SetMinimumLevel(LogLevel.Debug);
+            });
             var channel = GrpcChannel.ForAddress("https://localhost:5001",
-                new GrpcChannelOptions { HttpClient = httpClient });
+                new GrpcChannelOptions { LoggerFactory = loggerFactory });
 
             var client = new CustomerService.CustomerServiceClient(channel);
 
